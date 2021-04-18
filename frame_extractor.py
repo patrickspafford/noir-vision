70807d73592168c92b27bbf6e6bf223f997d6628
patrickspafford <patrickspafford1@gmail.com>
2021-04-18 15:51:06 -0500

    Frame extractor and pca

--- frame_extractor.py
+++ frame_extractor.py
@@ -1,43 +1,79 @@
 
-import cv2
-import os
+import cv2 as cv
+from os import makedirs
+from os.path import join, exists
 
-# Read the video from specified path
-cam = cv2.VideoCapture(
-    "C:\\Users\\Admin\\PycharmProjects\\project_1\\openCV.mp4")
 
-try:
+def extract_images_from_video(source_path, output_path, seconds_per_frame=1):
+    cam = cv.VideoCapture(source_path)
+    try:
+        if not exists(output_path):
+            makedirs(output_path)
+    except OSError:
+        print('Error when creating directory of extracted frames.')
+    count = 0
+    while(True):
+        ret, frame = cam.read()
+        if ret:
+            name = join(output_path, 'frame_' + str(count) + '.jpg')
+            print('Creating...' + name)
+            cv.imwrite(name, frame)
+            count += 30 * seconds_per_frame
+            cam.set(1, count)
+        else:
+            break
+    cam.release()
+    cv.destroyAllWindows()
 
-    # creating a folder named data
-    if not os.path.exists('data'):
-        os.makedirs('data')
 
-# if not created then raise error
-except OSError:
-    print('Error: Creating directory of data')
+def get_images_from_live_video(output_path, seconds_per_frame=1):
+    cam = cv.VideoCapture(0)
+    try:
+        if not exists(output_path):
+            makedirs(output_path)
+    except OSError:
+        print('Error when creating directory of extracted frames.')
+    count = 0
+    while(True):
+        _, frame = cam.read()
+        if count % (30 * seconds_per_frame) == 0:
+            name = join(output_path, 'frame_' + str(count) + '.jpg')
+            print('Creating...' + name)
+            cv.imwrite(name, frame)
+        count += 1
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    cam.release()
+    cv.destroyAllWindows()
 
-# frame
-currentframe = 0
 
-while(True):
+def run_trained_svm(output_path, seconds_per_frame, classifier, preprocessor, preprocessor_k=5):
+    cam = cv.VideoCapture(0)
+    try:
+        if not exists(output_path):
+            makedirs(output_path)
+    except OSError:
+        print('Error when creating directory of extracted frames.')
+    count = 0
+    while(True):
+        _, frame = cam.read()
+        if count % (30 * seconds_per_frame) == 0:
+            if preprocessor:
+                frame = preprocessor(frame, preprocessor_k)
+            if (classifier(frame)):
+                name = join(output_path, 'frame_' + str(count) + '.jpg')
+                print('Found person in...' + name)
+                cv.imwrite(name, frame)
+        count += 1
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    cam.release()
+    cv.destroyAllWindows()
 
-    # reading from frame
-    ret, frame = cam.read()
 
-    if ret:
-        # if video is still left continue creating images
-        name = './data/frame' + str(currentframe) + '.jpg'
-        print('Creating...' + name)
-
-        # writing the extracted images
-        cv2.imwrite(name, frame)
-
-        # increasing counter so that it will
-        # show how many frames are created
-        currentframe += 1
-    else:
-        break
-
-# Release all space and windows once done
-cam.release()
-cv2.destroyAllWindows()
+"""
+extract_images_from_video('/Users/patrickspafford/Desktop/Machine Learning/finalProject/sample_640x360.mp4',
+                          '/Users/patrickspafford/Desktop/Machine Learning/finalProject/samples', seconds_per_frame=1)
+"""
+get_images_from_live_video(
+    '/Users/patrickspafford/Desktop/Machine Learning/finalProject/samples', 2)
--- kmeans2.py
+++ kmeans2.py
@@ -20,6 +20,20 @@ def save_kmeans_image(source_path, k, output_path):
     cv.imwrite(output_path, result)
 
 
+def transform_kmeans(frame, k):
+    img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    vectorized_img = img.reshape((-1, 3))
+    new_vectorized_img = np.float32(vectorized_img)
+    stopping_criteria = (cv.TERM_CRITERIA_EPS +
+                         cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    attempts = 10
+    _, label, center = cv.kmeans(
+        new_vectorized_img, k, None, stopping_criteria, attempts, cv.KMEANS_PP_CENTERS)
+    center = np.uint8(center)
+    misshapen_result = center[label.flatten()]
+    return misshapen_result.reshape((img.shape))
+
+
 def save_kmeans_directory(source_folder, k, output_folder):
     image_files = [f for f in listdir(
         source_folder) if isfile(join(source_folder, f))]
